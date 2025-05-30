4842f10d4e2bed727b1ccd87a7174d121973f7e0
Ilhan Klisura <work@ilhanklisura.com>
2025-05-30 16:52:15 +0200

    Fix: Updated index.php file for CORS

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,17 @@
 
 require './vendor/autoload.php';
 
+// CORS headers
+header("Access-Control-Allow-Origin: https://seahorse-app-jta9z.ondigitalocean.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // Middleware i dodatni servisi
 require_once 'middleware/AuthMiddleware.php';
 require_once 'rest/services/AuthService.php';
