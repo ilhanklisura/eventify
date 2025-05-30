019e241eadc3e3040c674bfd47041690d700c080
Ilhan Klisura <work@ilhanklisura.com>
2025-05-30 16:42:51 +0200

    Fix: Updated production API base URL by removing '/backend' path

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -3,12 +3,12 @@
 require __DIR__ . '/../../../vendor/autoload.php';
 
 define('LOCALSERVER', 'http://localhost/eventify/backend/');
-define('PRODSERVER', 'https://squid-app-lnxkv.ondigitalocean.app/backend/');
+define('PRODSERVER', 'https://squid-app-lnxkv.ondigitalocean.app/');
 
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/eventify/backend/');
 } else {
-    define('BASE_URL', 'https://squid-app-lnxkv.ondigitalocean.app/backend/');
+    define('BASE_URL', 'https://squid-app-lnxkv.ondigitalocean.app/');
 }
 
 $openapi = \OpenApi\Generator::scan([
